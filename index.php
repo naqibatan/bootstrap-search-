9a3387986a11b0e2c9435f04ae8c6d8ac241d894
naqib.zenbook <naqibatan28@gmail.com>
2024-02-16 11:30:57 +0800

    adjust page 1.3

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
                                 <form action="" method="GET">
                                     <div class="input-group mb-3">
-                                        <input type="text" name="search" value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>" class="form-control" placeholder="Search Data">
+                                        <input type="text" name="search" required value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>" class="form-control" placeholder="Search Data">
                                         <button type="submit" class="btn btn-primary">click me bitch!</button>
                                     </div>
                                 </form>
@@ -44,11 +44,43 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php
+                                    $con = mysqli_connect("localhost","mydbadmin","","mydb1_");
 
+                                    if(isset($_GET['search']))
+                                    {
+                                        $filtervalues = $_GET['search'];
+                                        $query = "SELECT * FROM users WHERE CONCAT(firstname,lastname,email) LIKE '%$filtervalues%'";
+                                        $query_run = mysqli_query($con, $query);
+
+                                        if(mysqli_num_rows($query_run) > 0)
+                                        {
+                                            foreach($query_run as $items)
+                                            {
+                                                ?>
+                                                    <tr>
+                                                        <td><?= $items['id']; ?></td>
+                                                        <td><?= $items['firstname']; ?></td>
+                                                        <td><?= $items['lastname']; ?></td>
+                                                        <td><?= $items['email']; ?></td>
+                                                    </tr>
+                                                <?php
+                                            }
+                                        }
+                                        else
+                                        {
+                                            ?>
+                                                <tr>
+                                                    <td colspan="4">No Record Found</td>
+                                                </tr>
+                                            <?php
+                                        }
+                                    }
+                                ?>
                                 <tr>
                                     <td></td>
                                 </tr>
-                                
+
                             </tbody>
                         </table>
                     </div>
